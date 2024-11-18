bbf31edf00956feffc87cb06b5a66790313b190f
andrastoth021 <andrastoth021@gmail.com>
2024-11-18 12:00:03 +0100

    remove unused imports from app.ts

--- src/controller/app.ts
+++ src/controller/app.ts
@@ -2,11 +2,8 @@ import fastify from 'fastify';
 import { PetService } from '../service/pet.service';
 import { PetRepository } from '../repository/pet.repository';
 import { DbClient } from '../db';
-import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
-import { getPetByIdSchema, getPetsSchema, postPetsSchema, putPetsToOwnersSchema } from './pet.schemas';
 import { OwnerRepository } from '../repository/owner.repository';
 import { OwnerService } from '../service/owner.service';
-import { getOwnerByIdSchema, getOwnersSchema, postOwnerSchema } from './owner.schemas';
 import { petRoutes } from './routes/pet.routes';
 import { ownerRoutes } from './routes/owner.routes';
 
